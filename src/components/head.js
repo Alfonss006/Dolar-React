7a2a1cf4f48f70adb258008ba1ccfc66e3663805
Darwin Poblete <darwinalfonsop@gmail.com>
2020-06-25 19:04:08 -0400

    configure response 404

--- src/components/head.js
+++ src/components/head.js
@@ -8,18 +8,38 @@ class Cabeza extends Component {
         console.log('button was clicked')
     }
 
-
     render() {  
         return (
         <div>
             <div>
                 <h1> Valor del dolar hist&oacute;rico</h1>
             </div>
-            <input type="date"></input>
-                hasta
-            <input type="date"></input>
+            
+            <input 
+            max={new Date().toISOString().split("T")[0]} 
+            value={this.props.startDate} 
+            onChange={this.props.handleChange} 
+            type="date"
+            name="start"
+            />
+            
+            <span>Hasta</span>
+            
+            <input 
+            max={new Date().toISOString().split("T")[0]} 
+            value={this.props.startDate} 
+            onChange={this.props.handleChange} 
+            type="date"
+            name="end"
+            min={this.props.start}
+            ></input>
 
-            <input className="head_buttom" onClick={this.handleClick} type="button" value="Ver Valor"/>
+            <input 
+            className="head_buttom" 
+            onClick={this.props.getDolars} 
+            type="button" 
+            value="Ver Valor"
+            />
        </div>);
     }
 }
--- src/pages/observeDolar.js
+++ src/pages/observeDolar.js
@@ -6,33 +6,83 @@ import '../components/sass/head.sass';
 
 class ObserveDolar extends Component {
     BaseURL = 'https://api.sbif.cl/api-sbifv3/recursos_api/dolar/';
-    apiKey = '9c84db4d447c80c74961a72245371245cb7ac15f'
-    //periodo/2019/12/dias_i/21/2020/3/dias_f/07?
+    apiKey = '9c84db4d447c80c74961a72245371245cb7ac15f';
     state = { 
         datalabels: [],
         data:[],
         isLoading: true,
-        error: null
-     }
-     componentDidMount(){
-         this.getDolars()
+        error: null,
+        end: new Date().toISOString().split("T")[0],
+        start: null,
+        result: {
+            Mensaje: null,
+            Dolares: []
+        }
      }
 
-     getDolars = async () => {
-         this.setState({ isLoading: true, error: null })
-         
-         await fetch(this.BaseURL + 'periodo/2020/5/dias_i/1/2020/6/dias_f/08' + '?formato=json&apikey=' + this.apiKey)
-         .then(result => result.json())
-         .then( result => this.setState({isLoading: false, datalabels : result.Dolares.map(item => item.Fecha), data: result.Dolares.map(item => parseFloat(item.Valor)) }))
-         .catch(error => this.setState({isLoading:false, error: error}))
-        
-        }
+    getDolars = async () => {
+         this.setState({ isLoading: true, result: {Mensaje: null} })
+         try {
+         await fetch(this.BaseURL + 'periodo/'
+         + this.state.startYear + '/'
+         + this.state.startMonth+ '/dias_i/'
+         + this.state.startDay + '/'
+         + this.state.endYear + '/'
+         + this.state.endMonth + '/dias_f/'
+         + this.state.endDay + '?formato=json&apikey=' 
+         + this.apiKey)
+         .then(result => result.json(),(error)=> {
+            this.setState({isLoading:false, error: error})
+         })
+         .then(result => this.setState({result : result}), 
+           (error)=> {
+            this.setState({isLoading:false, error: error})
+         })
+         .catch (error => this.setState({isLoading:false, error: error}))
+         }catch(e){}
+         if(!this.state.result.Mensaje){
+             this.setState({isLoading: false, datalabels : this.state.result.Dolares.map(item => item.Fecha), data: this.state.result.Dolares.map(item => parseFloat(item.Valor)) })
+         }
+         this.render();
+    }
+
+    handleChange = e => {    
+        let date = new Date(e.target.value+"T00:00:00");
+        this.setState({
+            [e.target.name + "Day"] : date.getDate(),
+            [e.target.name + "Year"] : date.getFullYear(),
+            [e.target.name + "Month"] : date.getMonth(),
+            [e.target.name] : date.toISOString().split("T")[0]
+        });
+      };    
 
     render() { 
+
+        if(this.state.result.Mensaje){
+            console.dir(this.state.error)
+            return(
+                <div className="container">
+                    <Cabeza/>
+                   <p> error {this.state.result.Mensaje}</p>
+                </div>
+            )
+        }
         return (
             <div className="container">
-                <Cabeza /><br></br>
-                <Graph datalabels={this.state.datalabels} data={this.state.data}/>
+                
+                <Cabeza 
+                end={this.state.Date} 
+                handleChange={this.handleChange}
+                getDolars={this.getDolars}
+                start={this.state.start}
+                />
+                
+                <br></br>
+                
+                <Graph 
+                datalabels={this.state.datalabels} 
+                data={this.state.data}/>
+            
             </div>
          );
     }
